6302868bfdaae7d9e5446a38d157882b10a98125
ariannadrovandi <121503321+ariannadrovandi@users.noreply.github.com>
2023-07-03 15:25:34 +0200

    Revert "3 apartments single apartment card apartment"

--- src/views/ApartmentCard.vue
+++ src/views/ApartmentCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        SingleApartment
+        ApartmentCard
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SingleApartment',
+    name: 'ApartmentCard',
 }
 </script>
 
