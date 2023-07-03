b11c0465819a3daa6f003467f07aa91285356904
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-07-03 17:09:16 +0200

    first edit in homeview

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    ciao
+
   </div>
   <RouterView></RouterView>
 </template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="">
-    <div class="">
-      <img class="img-fluid" src="img\Foto-Aerea-Verona-1-min-scaled-1.jpg" alt="">
+    <div class="jumbotron">
+      <img class="img-fluid" src="img\2008720-bellissimo-paesaggio-urbano-di-kobe-citta-giappone-gratuito-foto.jpg"
+        alt="jumbo">
     </div>
   </div>
   <h2 class="text-capitalize">featured apartments</h2>
@@ -20,4 +21,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.jumbotron {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
