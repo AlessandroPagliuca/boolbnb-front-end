a115bc14c2c8cee7a315e0aa709da2ff7318aa6d
Giuseppe Indomenico <120144234+GiuseppeIndomenico@users.noreply.github.com>
2023-07-03 15:25:54 +0200

    Revert "2 home developers view"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="vh-100">
-    <img class="img-fluid" src="img\Foto-Aerea-Verona-1-min-scaled-1.jpg" alt="">
+  <div>
+    HomeView
   </div>
 </template>
-
-<h2 class="text-capitalize">featured apartments</h2>
-
-<!-- carosello -->
-
-<h2 class="text-capitalize">maps</h2>
-
-<!-- mappa -->
 
 <script>
 export default {
