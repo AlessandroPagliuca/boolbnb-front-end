a961409cb007974a81a2c81dd94b129ede193a11
Franz2610 <francescofroechlich1@gmail.com>
2023-07-04 12:11:33 +0200

    Developers

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+const teams= [
+    {
+      name: "Alessandro Pagliuca",
+      role: "Junior Back-end Developer",
+      link: "https://www.linkedin.com/in/alessandro-pagliuca/",
+      link2: "https://github.com/AlessandroPagliuca",
+      profile_img: "/public/team/alex.jpeg",
+    },
+    {
+      name: "Francesco Froechlich",
+      role: "Junior Full Stack Web Developer",
+      link: "https://www.linkedin.com/in/francesco-froechlich-2175b427b/",
+      link2: "https://github.com/Franz2610",
+      profile_img: "/public/team/francesco.jpg",
+    },
+    {
+      name: "Giuseppe Indomenico",
+      role: "Junior Full Stack Web Developer",
+      link: "https://www.linkedin.com/in/giuseppe-indomenico-29560914b/",
+      link2: "https://github.com/GiuseppeIndomenico",
+      profile_img: "/public/team/giuseppe.jpeg",
+    },
+    {
+      name: "Giovanni Speranza",
+      role: "Junior Back-end Developer",
+      link: "https://www.linkedin.com/in/giovanni-jacopo-speranza/",
+      link2: "https://github.com/giospes",
+      profile_img: "/public/team/giovanni.jpeg",
+    },
+    {
+      name: "Arianna Drovandi",
+      role: "Junior Front-end Developer",
+      link: " https://www.linkedin.com/in/arianna-drovandi-562990230/",
+      link2: "https://github.com/ariannadrovandi",
+      profile_img: "/public/team/arianna.jpeg",
+    },
+    {
+      name: "Anas Hamimi ",
+      role: "Junior Full Stack Web Developer",
+      link: "https://www.linkedin.com/in/anas-hamimi-514208280/",
+      link2: "https://github.com/anashamimi",
+      profile_img: "/public/team/anas.jpeg",
+    },
+  ]
+
+  export{teams}
--- src/pages/DevelopersView.vue
+++ src/pages/DevelopersView.vue
@@ -1,13 +1,73 @@
 <template>
-  <div>
-    DevelopersView
+  <div class="container 100vh">
+    <h2 class="text-center my-5 text-team">Conosci meglio il nostro Team!</h2>
+    <div class="row align-items-stretch">
+      <div v-for="(team, index) in teams" :key="index" class="col-12 col-md-6 col-lg-4">
+        <div  class="cards-container d-flex justify-content-center">
+          <div class="card rounded-2">
+            <div class="header-card">
+              <div class="avatar">
+                <img :src="team.profile_img" :alt="team.name" />
+              </div>
+            </div>
+
+            <h4 class="text-black">{{ team.name }}</h4>
+            <div class="">
+              <p class="fw-semibold">{{ team.role }}</p>
+              <hr />
+              <div class="d-flex">
+                <a class=" fw-semibold" href="{{ team.link }}">Linkedin</a>
+              <p class="px-1">|</p>
+              <a class=" fw-semibold" href="{{ team.link }}">Github</a>
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { teams } from "../data/data";
+// import DevelopersView from "../components/DevelopersView.vue";
+
 export default {
-  name: 'DevelopersView',
-}
+  name: "DevelopersView",
+  // components: {
+  //   DevelopersView,
+  // },
+  data() {
+    return {
+      teams,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/partials/variables' as *;
+img{
+  width: 280px;
+  border-radius: 100%;
+  margin: 10px;
+}
+.card{
+  width: 300px;
+  height: 430px;
+  margin-bottom: 50px;
+}
+.header-card{
+  background-color: $primary;
+  height: 300px;
+}
+.text-team{
+  color: $primary;
+}
+a{
+  text-decoration: none;
+}
+</style>
