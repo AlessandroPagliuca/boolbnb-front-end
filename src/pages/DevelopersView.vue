df033b622a011eb4f488b46ed4f3eacb73ffe1e8
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-07-05 12:33:48 +0200

    edit developer card

--- src/pages/DevelopersView.vue
+++ src/pages/DevelopersView.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="container 100vh">
-    <h2 class="text-center my-5 text-team">Conosci meglio il nostro Team!</h2>
-    <div class="row align-items-stretch">
-      <div v-for="(team, index) in teams" :key="index" class="col-12 col-md-6 col-lg-4">
-        <div class="cards-container d-flex justify-content-center">
-          <div class="card rounded-2">
-            <div class="header-card">
-              <div class="avatar ">
-                <img class="" :src="team.profile_img" :alt="team.name" />
-              </div>
-            </div>
+  <div class="background">
 
-            <h4 class="text-black px-2 pt-2">{{ team.name }}</h4>
-            <div>
-              <p class="fw-semibold px-2">{{ team.role }}</p>
-              <hr />
-              <div class="d-flex px-2">
-                <a class=" fw-semibold" href="{{ team.link }}">Linkedin <i class="fa-brands fa-linkedin text-black"></i>
-                </a>
-                <span class="px-1"> | </span>
-                <a class=" fw-semibold" href="{{ team.link }}">Github <i class="fa-brands fa-github text-black"></i></a>
+    <div class="container 100vh">
+      <h2 class="text-center my-5 text-team">Conosci meglio il nostro Team!</h2>
+      <div class="row align-items-stretch">
+        <div v-for="(team, index) in teams" :key="index" class="col-12 col-md-6 col-lg-4">
+          <div class="cards-container d-flex justify-content-center">
+            <div class="card rounded-2">
+              <div class="header-card">
+                <div class="avatar ">
+                  <img class="" :src="team.profile_img" :alt="team.name" />
+                </div>
+              </div>
+
+              <h4 class="text-black px-2 pt-2">{{ team.name }}</h4>
+              <div>
+                <p class="fw-semibold px-2">{{ team.role }}</p>
+                <hr />
+                <div class="d-flex px-2">
+                  <a class=" fw-semibold" href="{{ team.link }}">Linkedin <i class="fa-brands fa-linkedin text-black"></i>
+                  </a>
+                  <span class="px-1"> | </span>
+                  <a class=" fw-semibold" href="{{ team.link }}">Github <i class="fa-brands fa-github text-black"></i></a>
+                </div>
               </div>
             </div>
           </div>
@@ -51,7 +54,9 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/partials/variables' as *;
 
-
+.background {
+  background-color: $primary-subtle;
+}
 
 img {
   width: 280px;
@@ -65,16 +70,24 @@ img {
   margin-bottom: 50px;
   transition: all .5s;
   transform: scale(0.9);
+  border: transparent;
+  background-color: transparent;
 
 
   &:hover {
     transform: scale(1.1);
-    box-shadow: 1px 1px 12px 6px rgba(0, 0, 0, 0.3);
+    box-shadow: 12px 12px 12px 5px rgba($primary, $alpha: 0.3);
+    border: rgba(0, 0, 0, 0.3);
+
   }
 }
 
+hr {
+  color: $primary;
+}
+
 .header-card {
-  background-color: $primary;
+
   height: 300px;
   border-top-left-radius: 5px;
   border-top-right-radius: 5px;
