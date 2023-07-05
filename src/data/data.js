ef075204eee57566e2d65611d0be70cb27c78413
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-07-05 13:28:31 +0200

    fix developer page

--- src/data/data.js
+++ src/data/data.js
@@ -1,46 +1,46 @@
-const teams= [
-    {
-      name: "Alessandro Pagliuca",
-      role: "Junior Back-end Developer",
-      link: "https://www.linkedin.com/in/alessandro-pagliuca/",
-      link2: "https://github.com/AlessandroPagliuca",
-      profile_img: "/public/team/alex.jpeg",
-    },
-    {
-      name: "Francesco Froechlich",
-      role: "Junior Full Stack Web Developer",
-      link: "https://www.linkedin.com/in/francesco-froechlich-2175b427b/",
-      link2: "https://github.com/Franz2610",
-      profile_img: "/public/team/francesco2.jpeg",
-    },
-    {
-      name: "Giuseppe Indomenico",
-      role: "Junior Full Stack Web Developer",
-      link: "https://www.linkedin.com/in/giuseppe-indomenico-29560914b/",
-      link2: "https://github.com/GiuseppeIndomenico",
-      profile_img: "/public/team/giuseppe3.jpeg",
-    },
-    {
-      name: "Giovanni Speranza",
-      role: "Junior Back-end Developer",
-      link: "https://www.linkedin.com/in/giovanni-jacopo-speranza/",
-      link2: "https://github.com/giospes",
-      profile_img: "/public/team/giovanni.jpeg",
-    },
-    {
-      name: "Arianna Drovandi",
-      role: "Junior Front-end Developer",
-      link: " https://www.linkedin.com/in/arianna-drovandi-562990230/",
-      link2: "https://github.com/ariannadrovandi",
-      profile_img: "/public/team/arianna4.png",
-    },
-    {
-      name: "Anas Hamimi ",
-      role: "Junior Full Stack Web Developer",
-      link: "https://www.linkedin.com/in/anas-hamimi-514208280/",
-      link2: "https://github.com/anashamimi",
-      profile_img: "/public/team/anas.jpeg",
-    },
-  ]
+const teams = [
+  {
+    name: "Alessandro Pagliuca",
+    role: "Junior Back-end Developer",
+    link: "https://www.linkedin.com/in/alessandro-pagliuca/",
+    link2: "https://github.com/AlessandroPagliuca",
+    profile_img: "/team/alex.jpeg",
+  },
+  {
+    name: "Francesco Froechlich",
+    role: "Junior Full Stack Web Developer",
+    link: "https://www.linkedin.com/in/francesco-froechlich-2175b427b/",
+    link2: "https://github.com/Franz2610",
+    profile_img: "/team/francesco2.jpeg",
+  },
+  {
+    name: "Giuseppe Indomenico",
+    role: "Junior Full Stack Web Developer",
+    link: "https://www.linkedin.com/in/giuseppe-indomenico-29560914b/",
+    link2: "https://github.com/GiuseppeIndomenico",
+    profile_img: "/team/giuseppe3.jpeg",
+  },
+  {
+    name: "Giovanni Speranza",
+    role: "Junior Back-end Developer",
+    link: "https://www.linkedin.com/in/giovanni-jacopo-speranza/",
+    link2: "https://github.com/giospes",
+    profile_img: "/team/giovanni.jpeg",
+  },
+  {
+    name: "Arianna Drovandi",
+    role: "Junior Front-end Developer",
+    link: " https://www.linkedin.com/in/arianna-drovandi-562990230/",
+    link2: "https://github.com/ariannadrovandi",
+    profile_img: "/team/arianna4.png",
+  },
+  {
+    name: "Anas Hamimi ",
+    role: "Junior Full Stack Web Developer",
+    link: "https://www.linkedin.com/in/anas-hamimi-514208280/",
+    link2: "https://github.com/anashamimi",
+    profile_img: "/team/anas.jpeg",
+  },
+]
 
-  export{teams}
+export { teams }
