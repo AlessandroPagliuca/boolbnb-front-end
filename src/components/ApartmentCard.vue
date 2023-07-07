7603d750307248e5c01880650be6b44847ef4740
anashamimi <anashamimi2@gmail.com>
2023-07-07 09:42:58 +0200

    fix all

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -1,27 +1,25 @@
 <template>
-<<<<<<< HEAD
     <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-if="apartment.visible">
-=======
-    <div class="card shadow mb-3">
-        <div class="card-img-top">
->>>>>>> 6d8d06befc17cf63f8597b76e55ce0a5b7db95b8
+        <router-link :to="{ name: 'single-apartment', params: { slug: apartment.slug } }">
+            <div class="card mb-2 border">
+                <img class="img-fluid" style="height: 200px;" v-if="apartment.main_img.includes('http')"
+                    :src="apartment.main_img" alt="">
+                <img class="img-fluid" style="height: 200px;" v-else :src="getImagePath" :alt="apartment.title">
 
-            <img class="img-fluid" v-if="apartment.main_img.includes('http')" :src="apartment.main_img" alt="">
-            <img class="img-fluid" v-else :src="getImagePath" :alt="apartment.title">
-        </div>
-        <div class="card-body justify-content-between align-items-end">
-            <div>
-                <h6>{{ apartment.title }}</h6>
-                <span>rooms :{{ apartment.rooms }}</span>
-                <span> | </span>
-                <span>beds :{{ apartment.beds }}</span>
-                <p>{{ apartment.price }}: &euro;</p>
+                <div class="card-body justify-content-between align-items-end">
+                    <div>
+                        <h6>{{ apartment.title }}</h6>
+                        <span>rooms :{{ apartment.rooms }}</span>
+                        <span> | </span>
+                        <span>beds :{{ apartment.beds }}</span>
+                        <p>{{ apartment.price }}: &euro;</p>
+                    </div>
+                    <div class="card-text">
+
+                    </div>
+                </div>
             </div>
-            <div class="card-text">
-                <router-link :to="{ name: 'single-apartment', params: { slug: apartment.slug } }"
-                    class="btn btn-primary">Show the apartment</router-link>
-            </div>
-        </div>
+        </router-link>
     </div>
 </template>
 
@@ -48,5 +46,23 @@ export default {
 
 .card-img-top {
     width: 500px;
+}
+
+.card {
+    width: 300px;
+    height: 440px;
+    margin-bottom: 50px;
+    transition: all .5s;
+    border: transparent;
+    background-color: transparent;
+    transform: scale(0.9);
+
+
+    &:hover {
+        transform: scale(1);
+        box-shadow: 12px 12px 12px 5px rgba($primary, $alpha: 0.3);
+        border: rgba(0, 0, 0, 0.3);
+
+    }
 }
 </style>
--- src/pages/ApartmentsView.vue
+++ src/pages/ApartmentsView.vue
@@ -1,30 +1,9 @@
 <template>
-    <div class="body overflow-auto">
+    <div class="body">
         <div class="container">
             <SearchBar />
 
-            <!-- <h1 class="text-black my-5">{{ title }}</h1>
-            <div class="row mb-4">
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="(apartment, index) in apartments" :key="apartment.id">
-                    <ApartmentCard :apartment="apartment" />
-                </div>
-            </div>
 
-            <div class="" style="width: fit-content;">
-                <nav aria-label="Page navigation example" class="py-5">
-                    <ul class="pagination">
-                        <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 1 }"
-                                @click="getData(currentPage - 1)">Previous</button></li>
-                        <li class="page-item" v-for="n in lastPage">
-                            <button :class="{ 'page-link': true, 'active': currentPage === n }" @click="getData(n)">{{ n
-                            }}</button>
-                        </li>
-
-                        <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === lastPage }"
-                                @click="getData(currentPage + 1)">Next</button></li>
-                    </ul>
-                </nav>
-            </div> -->
         </div>
     </div>
 </template>
@@ -44,10 +23,6 @@ export default {
 <style lang="scss" scoped>
 .body{
     margin-top: 76px;
-    height: 100vh;
 }
 
-.container{
-    height: 100vh;
-}
 </style>
