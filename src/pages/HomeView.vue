6465ca2f3561fefcd567aab9d8d14306602923ce
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-07-10 16:24:07 +0200

    add media query button and router link jumbo

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -7,10 +7,17 @@
         <h2 class="text-capitalize display-1 fw-bold t-dark text-center">
           welcome to <br>
         </h2>
-
         <h1 class="text-uppercase fw-bolde fw-bolder t-dark display-1 ">boolBnB!</h1>
 
-
+        <div class="t-button shadow rounded-2"><router-link :to="{ name: 'apartments' }"
+            class="text-decoration-none text-light fw-bold d-flex justify-content-between align-items-center">
+            <div class="px-2">
+              See all
+              apartments
+            </div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </router-link>
+        </div>
 
       </div>
 
@@ -28,8 +35,10 @@
 
 
 <script>
+
 import Carousel from '../components/Carousel.vue';
 import MapComp from '../components/MapComp.vue';
+import ApartmentsView from './ApartmentsView.vue';
 //import SearchBar from '../components/SearchBar.vue'; 
 export default {
   name: 'HomeView',
@@ -68,7 +77,49 @@ export default {
 
   .t-dark {
     text-shadow: 5px 5px 3px rgba($color: #343a40, $alpha: 0.8);
-    color: #fb6f89;
+    color: $primary;
+  }
+
+  .t-button {
+    background-color: $primary;
+    width: 100%;
+    font-size: 8px;
+
+    i {
+      font-size: 18px;
+    }
+
+    @media screen {
+      padding: .5rem;
+      width: 75%;
+      font-size: 1rem;
+    }
+
+    @media (min-width: 676px) {
+      padding: 1rem;
+      width: 70%;
+      font-size: 18px;
+
+
+      i {
+        font-size: 24px;
+      }
+
+    }
+
+    @media (min-width:1024px) {
+      padding: 1rem;
+      width: 70%;
+      font-size: 24px;
+
+
+      i {
+        font-size: 36px;
+      }
+
+    }
+
+
   }
 }
 </style>
